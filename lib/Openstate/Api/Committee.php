4c7763a5a7a74c0eec4ff9c445564be3ef68de31
Josh <josh.williams@gmail.com>
2013-03-10 20:07:10 -0500

    Added a few committee methods

--- lib/Openstate/Api/Committee.php
+++ lib/Openstate/Api/Committee.php
@@ -4,5 +4,53 @@ namespace Openstate\Api;
 
 class Committee extends Api
 {
+    protected $state;
+    protected $chamber;
 
+    /**
+     * Fetches committee data
+     *
+     * @param   string  $committeeId
+     * @return  array
+     **/
+    public function get($committeeId)
+    {
+        return $this->request('committees/'.$committeeId.'/');
+    }
+
+    /**
+     * Finds all committees for the current state
+     *
+     * @return array
+     **/
+    public function findAll()
+    {
+        return $this->request('committees/', array('state' => $this->state, 'chamber' => $this->chamber));
+    }
+
+    /**
+     * Set state to use with api calls
+     *
+     * @param   string  $state
+     * @return  Committee
+     **/
+    public function state($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Set chamber to use with api calls
+     *
+     * @param   string  $state
+     * @return  Committee
+     **/
+    public function chamber($chamber)
+    {
+        $this->chamber = $chamber;
+
+        return $this;
+    } 
 }
